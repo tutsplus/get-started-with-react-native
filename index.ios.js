802fee0bf63b5f180f004d7ed7dd7c411b4b7b1c
Markus Mühlberger <markus@mmuehlberger.com>
2015-11-26 11:32:54 +0100

    App initialization and architecture

---
+++ index.ios.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  View,
+} = React;
+
+var iTunesBrowser = React.createClass({
+  render: function() {
+    return (
+      <View>
+      </View>
+    );
+  }
+});
+
+AppRegistry.registerComponent('iTunesBrowser', () => iTunesBrowser);
