9754bcd90f8521b92710a8016fcfe233652ed805
Markus Mühlberger <markus@mmuehlberger.com>
2017-04-02 14:08:32 +0200

    Dynamic Changes with State

--- components/run-info.js
+++ components/run-info.js
@@ -4,11 +4,22 @@ import { Text, View } from 'react-native';
 import sharedStyles from '../shared-styles';
 
 export default class RunInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: this.props.value };
+  }
+
+  formatValue() {
+    return this.state.value;
+  }
+
   render() {
+    let value = this.state.value ? this.formatValue() : '–';
+
     return (
       <View style={[sharedStyles.runInfoWrapper, {flex: 1, flexDirection: 'column-reverse'}]}>
         <Text style={sharedStyles.runInfoTitle}>{this.props.title.toUpperCase()}</Text>
-        <Text style={sharedStyles.runInfoValue}>{this.props.value}</Text>
+        <Text style={sharedStyles.runInfoValue}>{value}</Text>
       </View>
     );
   }
--- index.ios.js
+++ index.ios.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
 import RunInfo from './components/run-info';
+import RunInfoNumeric from './components/run-info-numeric';
 
 const styles = StyleSheet.create({
   infoWrapper: {
@@ -15,14 +16,34 @@ const styles = StyleSheet.create({
 });
 
 export default class GetStarted extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    setInterval(() => {
+      this.distanceInfo.setState({ value: Math.random() * 100 });
+      this.speedInfo.setState({ value: Math.random() * 15 });
+      this.directionInfo.setState({
+        value: this.directionInfo.state === 'N' ? 'NW' : 'N'
+      });
+    }, 1000);
+  }
+
   render() {
     return (
       <View style={{flex: 1}}>
         <Text style={{flex: 1, backgroundColor: 'red'}}>MAPVIEW</Text>
         <View style={styles.infoWrapper}>
-          <RunInfo title="Distance" value="0 km" />
-          <RunInfo title="Speed" value="0 km/h" />
-          <RunInfo title="Direction" value="NE" />
+          <RunInfoNumeric title="Distance" unit="km"
+            ref={(info) => this.distanceInfo = info}
+          />
+          <RunInfoNumeric title="Speed" unit="km/h"
+            ref={(info) => this.speedInfo = info}
+          />
+          <RunInfo title="Direction"
+            value="NE"
+            ref={(info) => this.directionInfo = info}
+          />
         </View>
       </View>
     );
