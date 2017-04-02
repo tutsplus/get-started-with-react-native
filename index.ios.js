4107ccc67dac19e08728b7fb187ec494bd48843d
Markus Mühlberger <markus@mmuehlberger.com>
2017-04-02 14:24:43 +0200

    Event Handling with TextInput and Buttons

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native';
 
 import RunInfo from './components/run-info';
 import RunInfoNumeric from './components/run-info-numeric';
@@ -12,6 +12,12 @@ const styles = StyleSheet.create({
     right: 0,
     flexDirection: 'row',
     flex: 1
+  },
+  textInput: {
+    height: 55,
+    padding: 10,
+    paddingTop: 25,
+    fontSize: 20
   }
 });
 
@@ -32,7 +38,22 @@ export default class GetStarted extends Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        <Text style={{flex: 1, backgroundColor: 'red'}}>MAPVIEW</Text>
+        <TextInput
+          style={styles.textInput}
+          autoCapitalize="words"
+          placeholder="Type something..."
+          onChangeText={(text) => this.setState({ text })}
+          onBlur={() => this.setState({ text: this.state.text.toUpperCase() })}
+        />
+        <TouchableHighlight
+          onPress={() => this.setState({ confirmedText: this.state.text })}
+          onLongPress={() => this.setState({ confirmedText: this.state.text.toLowerCase() })}
+        >
+          <Text style={{fontSize: 16, backgroundColor: '#EEE', textAlign: 'center'}}>Press Me!</Text>
+        </TouchableHighlight>
+        <Text style={{flex: 1, fontSize: 18}}>
+          {this.state.confirmedText}
+        </Text>
         <View style={styles.infoWrapper}>
           <RunInfoNumeric title="Distance" unit="km"
             ref={(info) => this.distanceInfo = info}
