ec43b5c446933e4d9d3e5bd1e3d4c3b940a1a717
Markus Mühlberger <markus@mmuehlberger.com>
2017-04-02 13:54:39 +0200

    Stylesheets and Flexbox

--- components/run-info.js
+++ components/run-info.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+import sharedStyles from '../shared-styles';
+
 export default class RunInfo extends Component {
   render() {
     return (
-      <View>
-        <Text>{this.props.title}</Text>
-        <Text>{this.props.value}</Text>
+      <View style={[sharedStyles.runInfoWrapper, {flex: 1, flexDirection: 'column-reverse'}]}>
+        <Text style={sharedStyles.runInfoTitle}>{this.props.title.toUpperCase()}</Text>
+        <Text style={sharedStyles.runInfoValue}>{this.props.value}</Text>
       </View>
     );
   }
--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
 import RunInfo from './components/run-info';
+
+const styles = StyleSheet.create({
+  infoWrapper: {
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    right: 0,
+    flexDirection: 'row',
+    flex: 1
+  }
+});
 
 export default class GetStarted extends Component {
   render() {
     return (
-      <View>
-        <Text>MAPVIEW</Text>
-        <View>
+      <View style={{flex: 1}}>
+        <Text style={{flex: 1, backgroundColor: 'red'}}>MAPVIEW</Text>
+        <View style={styles.infoWrapper}>
           <RunInfo title="Distance" value="0 km" />
           <RunInfo title="Speed" value="0 km/h" />
           <RunInfo title="Direction" value="NE" />
